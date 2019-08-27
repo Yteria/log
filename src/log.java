56eb947533c25346c1e6b6ff742fd449df4a61f4
Yteria <anton.spigerhall@elev.ga.ntig.se>
2019-08-27 11:08:36 +0200

    Started reed and write functions

--- src/log.java
+++ src/log.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class log {
     public static void main(String[] args) {
-        
+        int w = 1;
+        Scanner log = new Scanner(System.in);
+        ArrayList<String> logs = new ArrayList<>();
+        while (w > 0) {
+            int y = 0;
+            System.out.println("Please select a option");
+            System.out.println("1. Write post");
+            System.out.println("2. Reed logs");
+            int y = log.nextInt();
+            if (y == 1){
+                String post = log.nextLine();
+                logs.add(post);
+            } else if (y == 2) {
+                System.out.println(logs);
+            }
+
+        }
     }
 }
