b18eddc73e9bc0504f876e955087c5f8d224f748
Yteria <anton.spigerhall@elev.ga.ntig.se>
2019-09-03 09:37:06 +0200

    Finshed menu

--- src/log.java
+++ src/log.java
@@ -9,14 +9,33 @@ public class log {
         while (w > 0) {
             PrintMenu();
             int y = log.nextInt();
-            if (y == 1){
-                String post = log.nextLine();
-                logs.add(post);
-            } else if (y == 2) {
-                System.out.println(logs);
-            }
+            log.nextLine();
+            w = Answer(w, log, logs, y);
 
         }
+    }
+
+    private static int Answer(int w, Scanner log, ArrayList<String> logs, int y) {
+        if (y == 1){
+            System.out.println("Please write a post");
+            String post = log.nextLine();
+            logs.add(post);
+        } else if (y == 2) {
+            System.out.println("Current logs");
+            System.out.println(logs);
+        } else  if (y == 3) {
+            System.out.println("Edit post");
+
+        } else if (y == 4) {
+            System.out.println("Save");
+        } else if (y == 5) {
+            System.out.println("Load");
+        } else if (y == 6) {
+            w = 0;
+        } else {
+            System.out.println("Illegal action");
+        }
+        return w;
     }
 
     private static void PrintMenu() {
